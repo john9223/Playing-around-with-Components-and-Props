94abca305817bc3fe4709fbcbf2c5f5c37417001
john9223 <95772706+john9223@users.noreply.github.com>
2024-05-04 01:40:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import GallaryFooter from "./components/GallaryFooter";
+import GallaryHeader from "./components/GallaryHeader";
+import GallaryBody from "./components/GallaryBody";
+
+function App() {
+  return (
+    <div>
+      <GallaryHeader />
+      <GallaryBody />
+      <GallaryFooter />
+    </div>
+  );
+}
+
+export default App;
